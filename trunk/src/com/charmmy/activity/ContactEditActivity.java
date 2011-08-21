50e0d3d0e493dd9b16db0c14e26271074dec0b8d
jamesliulyc@gmail.com <jamesliulyc@gmail.com@e90d84de-5495-01c3-e1c4-69a26605bb6a>
2011-08-21 17:51:59 +0000

    新建联系人初步界面，底部按钮一直在软键盘上面，且不挡住输入框

---
+++ trunk/src/com/charmmy/activity/ContactEditActivity.java
@@ -0,0 +1,22 @@
+package com.charmmy.activity;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Window;
+
+/*
+ * James Liu
+ * 新建联系人
+ */
+public class ContactEditActivity extends Activity {
+	
+	@Override
+    public void onCreate(Bundle savedInstanceState) {
+		 super.onCreate(savedInstanceState);
+		 requestWindowFeature(Window.FEATURE_NO_TITLE);
+
+	     setContentView(R.layout.contact_edit);
+	}
+	
+
+}
